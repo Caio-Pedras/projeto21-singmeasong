18f31535a2b75d212699a16eabd790ff32424b21
CaioPedras <caio.vp@gmail.com>
2022-09-23 17:43:21 -0300

    feat:tests for GET /recommendations  && POST votes

--- back-end/tests/integration/recommendations.test.ts
+++ back-end/tests/integration/recommendations.test.ts
@@ -1,12 +1,17 @@
 import supertest from "supertest";
+import { resourceLimits } from "worker_threads";
 import app from "../../src/app.js";
 import { recommendationRepository } from "../../src/repositories/recommendationRepository.js";
 import recommendationFactory from "../factories/recommendationFactory.js";
-
-describe("/recomendations", () => {
-  beforeEach(async () => {
-    await recommendationRepository.resetDataBase();
-  });
+import {
+  createAnyNumberRecommendationScenario,
+  createAnyNumberScoredRecommendationScenario,
+  createOneRecomendationScenario,
+} from "../factories/scenarioFactory.js";
+beforeEach(async () => {
+  await recommendationRepository.resetDataBase();
+});
+describe("POST /recomendations", () => {
   it("Create a recommendation", async () => {
     const recommendation = recommendationFactory();
     const response = await supertest(app)
@@ -18,7 +23,65 @@ describe("/recomendations", () => {
       recommendation.name
     );
     expect(newRecommendation).not.toBeNull();
+  });
+});
 
-    it;
+describe("POST /recomendations/:id/upvote||downvote", () => {
+  it("Insert upvote on recommendation", async () => {
+    const recommendation = await createOneRecomendationScenario();
+    const { id } = await recommendationRepository.findByName(
+      recommendation.name
+    );
+    const response = await supertest(app).post(`/recommendations/${id}/upvote`);
+    expect(response.status).toBe(200);
+
+    const { score } = await recommendationRepository.find(id);
+    expect(score).toBe(1);
+  });
+  it("Insert downvote on recommendation", async () => {
+    const recommendation = await createOneRecomendationScenario();
+    const { id } = await recommendationRepository.findByName(
+      recommendation.name
+    );
+    const response = await supertest(app).post(
+      `/recommendations/${id}/downvote`
+    );
+    expect(response.status).toBe(200);
+
+    const { score } = await recommendationRepository.find(id);
+    expect(score).toBe(-1);
+  });
+});
+
+describe("GET /recomendations", () => {
+  it("Get all recommendations", async () => {
+    const SCENARIO = 10;
+    await createAnyNumberRecommendationScenario(SCENARIO);
+    const response = await supertest(app).get("/recommendations");
+    expect(response.status).toBe(200);
+    expect(response.body.length).toBe(SCENARIO);
+  });
+  it("Get recommendation by Id", async () => {
+    const recommendation = await createOneRecomendationScenario();
+    const { id } = await recommendationRepository.findByName(
+      recommendation.name
+    );
+    const response = await supertest(app).get(`/recommendations/${id}`);
+    expect(response.status).toBe(200);
+    expect(response.body).not.toBeNull();
+  });
+  it("get recommendations and order by score", async () => {
+    const SCENARIO = 10;
+    await createAnyNumberScoredRecommendationScenario(SCENARIO);
+    const response = await supertest(app).get(
+      `/recommendations/top/${SCENARIO}`
+    );
+    expect(response.status).toBe(200);
+    expect(response.body.length).toBe(SCENARIO);
+    for (let i = 0; i < SCENARIO - 1; i++) {
+      expect(response.body[i].score).toBeGreaterThanOrEqual(
+        response.body[i + 1].score
+      );
+    }
   });
 });
